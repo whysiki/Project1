fadecda3ac072058958f92c1a4cfedd57431432f
whysiki <whysiki@proton.me>
2024-10-21 03:16:16 +0800

    comment

--- Comment_cmd.py
+++ Comment_cmd.py
@@ -19,6 +19,7 @@ ElementBGArray_IM = {}
 
 # Form 'Form_1's Event :Load
 def Form_1_onLoad(uiName):
+    value = Fun.DelAllLines(uiName, "ListBox_1")
     selected_movie = global_statue.selected_movie
     comment_num = selected_movie.comment_num
     if int(comment_num) > 0:
@@ -27,34 +28,40 @@ def Form_1_onLoad(uiName):
         Fun.MessageBox("没有评论数据", "", "info", None)
 
 
-async def generate_comment(num: int = 20):
+async def generate_comment(num):
     selected_movie = global_statue.selected_movie
     index = num
-    while index > 0:
-        async for comment in selected_movie.get_comments():
-            # 用户信息
-            Fun.AddItemText(
-                uiName,
-                "ListBox_1",
-                f"{comment.user} {comment.rating} {comment.time} {comment.location}",
-                "end",
-            )
-            Fun.SetItemFGColor(uiName, "ListBox_1", "end", "#008000")
-            Fun.AddItemText(
-                uiName,
-                "ListBox_1",
-                f"{comment.content}",
-                "end",
-            )
-            Fun.SetItemFGColor(uiName, "ListBox_1", "end", "#000000")
-            Fun.AddItemText(
-                uiName,
-                "ListBox_1",
-                "",
-                "end",
-            )
-            index -= 1
+    async for comment in selected_movie.get_comments():
+        if index <= 0:
+            break
+        # 用户信息
+        Fun.AddItemText(
+            uiName,
+            "ListBox_1",
+            f"{comment.user} {comment.rating} {comment.time} {comment.location}",
+            "end",
+        )
+        Fun.SetItemFGColor(uiName, "ListBox_1", "end", "#008000")
+        Fun.AddItemText(
+            uiName,
+            "ListBox_1",
+            f"{comment.content}",
+            "end",
+        )
+        Fun.SetItemFGColor(uiName, "ListBox_1", "end", "#000000")
+        Fun.AddItemText(
+            uiName,
+            "ListBox_1",
+            "",
+            "end",
+        )
+        index -= 1
 
 
-def run_async_task(num: int = 20):
+def run_async_task(num):
     asyncio.run(generate_comment(num))
+
+
+# ListBox 'ListBox_1's MouseWheel Event :
+def ListBox_1_onMouseWheel(event, uiName, widgetName, threadings=0):
+    threading.Thread(target=run_async_task, args=(1,)).start()
