e56dde15be9e5aa2f93978dc1540b5709ac75d2f
whysiki <whysiki@proton.me>
2024-10-21 19:07:29 +0800

    fix comment

--- Comment_cmd.py
+++ Comment_cmd.py
@@ -16,14 +16,20 @@ ElementBGArray = {}
 ElementBGArray_Resize = {}
 ElementBGArray_IM = {}
 
+# 全局事件循环
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
-# Form 'Form_1's Event :Load
+task_queue = asyncio.Queue()
+
+
 def Form_1_onLoad(uiName):
     value = Fun.DelAllLines(uiName, "ListBox_1")
     selected_movie = global_statue.selected_movie
     comment_num = selected_movie.comment_num
     if int(comment_num) > 0:
-        threading.Thread(target=run_async_task, args=(10,)).start()
+        task_queue.put_nowait(10)
+        threading.Thread(target=start_async_task_from_queue).start()
     else:
         Fun.MessageBox("没有评论数据", "", "info", None)
 
@@ -32,35 +38,45 @@ async def generate_comment(num):
     selected_movie = global_statue.selected_movie
     index = num
     async for comment in selected_movie.get_comments():
-        # 用户信息
-        Fun.AddItemText(
+        loop.call_soon_threadsafe(
+            Fun.AddItemText,
             uiName,
             "ListBox_1",
             f"{comment.user} {comment.rating} {comment.time} {comment.location}",
             "end",
         )
-        Fun.SetItemFGColor(uiName, "ListBox_1", "end", "#008000")
-        Fun.AddItemText(
-            uiName,
-            "ListBox_1",
-            f"{comment.content}",
-            "end",
+        loop.call_soon_threadsafe(
+            Fun.SetItemFGColor, uiName, "ListBox_1", "end", "#008000"
         )
-        Fun.SetItemFGColor(uiName, "ListBox_1", "end", "#000000")
-        Fun.AddItemText(
-            uiName,
-            "ListBox_1",
-            "",
-            "end",
+        loop.call_soon_threadsafe(
+            Fun.AddItemText, uiName, "ListBox_1", f"{comment.content}", "end"
         )
+        loop.call_soon_threadsafe(
+            Fun.SetItemFGColor, uiName, "ListBox_1", "end", "#000000"
+        )
+        loop.call_soon_threadsafe(Fun.AddItemText, uiName, "ListBox_1", "", "end")
         index -= 1
         if index <= 0:
             return
 
 
-def run_async_task(num):
-    asyncio.run(generate_comment(num))
+async def run_async_task(num):
+    await generate_comment(num)
+
+
+def start_async_task(num):
+    loop.call_soon_threadsafe(asyncio.create_task, run_async_task(num))
+
+
+def start_async_task_from_queue():
+    while not task_queue.empty():
+        num = task_queue.get_nowait()
+        start_async_task(num)
 
 
 def ListBox_1_onMouseWheel(event, uiName, widgetName, threadings=0):
-    threading.Thread(target=run_async_task, args=(10,)).start()
+    task_queue.put_nowait(3)
+    threading.Thread(target=start_async_task_from_queue).start()
+
+
+threading.Thread(target=loop.run_forever).start()
